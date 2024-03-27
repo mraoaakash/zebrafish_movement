14136977b52b154df11fde0f4751639ba8e0d833
Aakash Rao <mrao.aakash@gmail.com>
2024-03-27 11:13:16 +0530

    added base files

---
+++ larval/train_script.py
@@ -0,0 +1,42 @@
+import comet_ml
+import sys
+from ultralytics import YOLO
+import argparse
+
+def trainer(
+        data, 
+        epochs, 
+        imgsz, 
+        name, 
+        batch, 
+        save_period, 
+        device, 
+        rect, 
+        workers
+        ):
+    model = YOLO('yolov8m.pt') 
+    results = model.train(
+        data=data,
+        epochs=int(epochs), 
+        imgsz=int(imgsz),
+        name=name, 
+        batch=int(batch),
+        save_period=int(save_period),
+        device=device,
+        rect=int(rect),
+        workers=int(workers)
+        )
+    
+if __name__ == '__main__':
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument('--data', type=str, help='data.yaml file path')
+    argparse.add_argument('--epochs', type=int, help='number of epochs')
+    argparse.add_argument('--imgsz', type=int, help='image size')
+    argparse.add_argument('--name', type=str, help='name of the model')
+    argparse.add_argument('--batch', type=int, help='batch size')
+    argparse.add_argument('--save_period', type=int, help='save period')
+    argparse.add_argument('--device', type=str, help='device')
+    argparse.add_argument('--rect', type=int, help='rect')
+    argparse.add_argument('--workers', type=int, help='workers')
+    args = argparse.parse_args()
+    trainer(args.data, args.epochs, args.imgsz, args.name, args.batch, args.save_period, args.device, args.rect, args.workers)
---
+++ run.sh
@@ -0,0 +1,10 @@
+python3 larval/train_script.py \
+    data=/Users/mraoaakash/Documents/research/research-zebrafish/Movement/zebrafish_movement/larval/larvae/data.yaml  \
+    epochs=50  \
+    imgsz=640  \
+    name=First-Run  \
+    batch=8  \
+    save_period=10 \
+    device=mps,  \
+    rect=True  \
+    workers=0 \
